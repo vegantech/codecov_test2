750d801957d26b73e695c18cb778aac5354ccb98
Shawn Balestracci <shawn@roshreview.com>
2016-02-15 19:04:11 -0500

    try -1 instead of nil

---
+++ lib/forgotten.rb
@@ -0,0 +1,5 @@
+def lonely
+  puts 'no one calls me'
+  puts 'but I should not be forgetten'
+  puts 'Coverage should be low, not 100% without me'
+end
---
+++ spec/lib/another_test_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+RSpec.describe true do
+  it 'has a test' do
+    expect(true).to be_truthy
+  end
+end
